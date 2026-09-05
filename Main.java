3a546e972774031dafc0ae5d5841a1d6fa503187
chaitanya014-code <chaitanyaec014.rymec@gmail.com>
2026-09-05 17:49:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    
+public static void main(String[] args) {
+
+        int[] numbers = {2, 7, 11, 15, 3, 6};
+        int target = 9;
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int number : numbers) {
+
+            int complement = target - number;
+
+            if (set.contains(complement)) {
+                System.out.println(
+                    complement + " + " + number + " = " + target
+                );
+            }
+
+            set.add(number);
+        }
+
+        System.out.println(set);
+    }
+}
